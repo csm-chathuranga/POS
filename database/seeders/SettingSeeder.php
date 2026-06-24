21a49a4520ccca2a2ada7d20bf505f828b7fa177
chathuranga <chaturanga@itone.biz>
2026-06-24 11:34:03 +0530

    fff

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -10,13 +10,22 @@ class SettingSeeder extends Seeder
     public function run(): void
     {
         $defaults = [
-            'shop_name'      => 'LMUC Convenience Store',
-            'shop_address'   => 'No. 1, Main Street, Colombo',
-            'shop_phone'     => '0112345678',
-            'shop_email'     => 'info@lmucstore.lk',
-            'currency'       => 'Rs.',
-            'tax_rate'       => '0',
-            'receipt_footer' => "ගෙවීම් සඳහා ස්තූතියි!\nThank you for shopping with us!",
+            'shop_name'          => 'LMUC Convenience Store',
+            'shop_address'       => 'No. 1, Main Street, Colombo',
+            'shop_phone'         => '0112345678',
+            'shop_email'         => 'info@lmucstore.lk',
+            'currency'           => 'Rs.',
+            'tax_rate'           => '0',
+            'receipt_footer'     => "ගෙවීම් සඳහා ස්තූතියි!\nThank you for shopping with us!",
+            'ui_language'        => 'si',
+            'bill_language'      => 'si',
+            'sidebar_theme'      => 'slate',
+            'primary_color'      => 'blue',
+            'barcode_label_size' => '40x25',
+            'barcode_show_price' => '1',
+            'logo'               => '',
+            'demo_mode'          => '0',
+            'printer_name'       => '',
         ];
 
         foreach ($defaults as $key => $value) {
